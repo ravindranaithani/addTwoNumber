4c924df88f22844268269189c8f692e558ca98bd
ravindranaithani <92716697+ravindranaithani@users.noreply.github.com>
2023-12-07 21:47:31 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,7 +2,8 @@ import streamlit as st
 st.title('Multiply Two Number')
 first_number = st.number_input('Enter the first number')
 second_number = st.number_input('Enter the second number')
-def ultiply(first, second):
+def multiply(first, second):
     result = first * second
     return result
+st.write(" This is the result":multiply(first, second))
   
