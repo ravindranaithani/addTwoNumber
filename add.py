09bc70990b7d37ae20e558bc0f336a75b6f0fc75
ravindranaithani <92716697+ravindranaithani@users.noreply.github.com>
2023-12-07 21:54:20 +0530

    Update add.py

--- add.py
+++ add.py
@@ -5,5 +5,5 @@ second_number = st.number_input('Enter the second number')
 def multiply(first_number, second_number):
     result = first_number * second_number
     return result
-st.write(" This is the result:" ,multiply(first, second_number))
+st.write(" This is the result:" ,multiply(first_number, second_number))
   
